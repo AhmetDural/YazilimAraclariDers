f0c8f1e0968444561ddcd0af36d087fb058a2d2c
Ahmet <ahmet.dural1903@gmail.com>
2020-12-16 21:35:19 +0300

    Ders çalışması 3

--- Ders2/src/test/java/tr/edu/medipol/yazilimaraclari/harfSaydırmaTest.java
+++ Ders2/src/test/java/tr/edu/medipol/yazilimaraclari/harfSaydırmaTest.java
@@ -2,14 +2,14 @@ package tr.edu.medipol.yazilimaraclari;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
- public class harfSaydirmaTest {
+ public class harfSaydýrmaTest {
 
 	@Test
 	public void testHarfleriSaydir() {
         
         String cumle = "Medipol Universitesi";
         
-        int sonuc = harfSaydirma.harfleriSaydir(cumle);
+        int sonuc = harfSaydýrma.harfleriSaydir(cumle);
         
         assertEquals(19, sonuc);
     }
